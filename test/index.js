535024fcfcac7da3878e6bb2d40e3449e596e44e
Max Rittmüller <max.rittmueller@gmail.com>
2016-01-08 23:29:01 +0100

    Fix: Windows mixing backslashes and slashes problem

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ var assert = require('assert'),
   parent = require('../lib/index.js'),
   mock = require('mock-fs'),
   fs = require('fs'),
+  path = require('path'),
   expect = require('chai').expect;
 
 var mockFs = {
@@ -24,13 +25,13 @@ mock(mockFs);
 
 describe('Finding files', function() {
   it('should find a package.json in the parent directory', function() {
-    expect(parent('test/first_folder/second_folderthird_folder').path).to.equal('test/first_folder/package.json');
+    expect(parent('test/first_folder/second_folderthird_folder').path).to.equal(path.normalize('test/first_folder/package.json'));
   });
   it('should find the parent package.json and ignore its own', function() {
-    expect(parent('test/first_folder/second_folder/third_folder/fourth_folder').path).to.equal('test/first_folder/second_folder/third_folder/package.json');
+    expect(parent('test/first_folder/second_folder/third_folder/fourth_folder').path).to.equal(path.normalize('test/first_folder/second_folder/third_folder/package.json'));
   });
   it('should continue until a package.json is found', function() {
-    expect(parent('test/first_folder/second_folder/third_folder').path).to.equal('test/first_folder/package.json');
+    expect(parent('test/first_folder/second_folder/third_folder').path).to.equal(path.normalize('test/first_folder/package.json'));
   });
   it('should equal to false if no package.json can be found', function() {
     expect(parent('test/first_folder')).to.equal(false);
@@ -39,14 +40,14 @@ describe('Finding files', function() {
 
 describe('Reading content', function() {
   it('should be able to read a package.json', function() {
-    expect(parent('test/first_folder/second_folder/third_folder').path).to.equal('test/first_folder/package.json');
+    expect(parent('test/first_folder/second_folder/third_folder').path).to.equal(path.normalize('test/first_folder/package.json'));
     expect(parent('test/first_folder/second_folder/third_folder').read()).to.equal('{"version":"2.0.0"}');
   });
 });
 
 describe('Parsing content', function() {
   it('should be able to read and parse a package.json', function() {
-    expect(parent('test/first_folder/second_folder/third_folder').path).to.equal('test/first_folder/package.json');
+    expect(parent('test/first_folder/second_folder/third_folder').path).to.equal(path.normalize('test/first_folder/package.json'));
     expect(parent('test/first_folder/second_folder/third_folder').parse()).to.eql({'version': '2.0.0'});
     expect(parent('test/first_folder/second_folder/third_folder').parse().version).to.equal('2.0.0');
   });
