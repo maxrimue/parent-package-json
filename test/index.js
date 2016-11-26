ffddcbd48d7341c1213cf0721cbc89ee47230e23
Max Rittmüller <max.rittmueller@gmail.com>
2016-11-26 22:08:25 +0100

    fix: paths for fixtures

--- test/index.js
+++ test/index.js
@@ -3,19 +3,19 @@ const test = require('ava').test;
 const parent = require('../lib/index.js');
 
 test('find right package.json', t => {
-	t.is(parent('./fixtures/firstFolder/secondFolder').path, path.normalize('fixtures/firstFolder/package.json'));
+	t.is(parent('./test/fixtures/firstFolder/secondFolder').path, path.normalize('test/fixtures/firstFolder/package.json'));
 });
 
 test('ignore one package.json', t => {
-	t.is(parent('./fixtures/firstFolder/secondFolder/thirdFolder/fourthFolder', 1).path, path.normalize('fixtures/firstFolder/package.json'));
+	t.is(parent('./test/fixtures/firstFolder/secondFolder/thirdFolder/fourthFolder', 1).path, path.normalize('test/fixtures/firstFolder/package.json'));
 });
 
 test('read package.json', t => {
-	t.deepEqual(JSON.parse(parent('./fixtures/firstFolder/secondFolder').read()), {version: '1.0.0'});
+	t.deepEqual(JSON.parse(parent('./test/fixtures/firstFolder/secondFolder').read()), {version: '1.0.0'});
 });
 
 test('parse package.json', t => {
-	t.deepEqual(parent('./fixtures/firstFolder/secondFolder').parse(), {version: '1.0.0'});
+	t.deepEqual(parent('./test/fixtures/firstFolder/secondFolder').parse(), {version: '1.0.0'});
 });
 
 test('fail at finding package.json', t => {
