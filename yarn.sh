11fe82a7f9b8f684258959f08f41f1e76ecfc972
Max Rittmüller <max.rittmueller@gmail.com>
2016-12-02 11:28:54 +0100

    test

--- yarn.sh
+++ yarn.sh
@@ -11,6 +11,10 @@ git checkout $TRAVIS_PULL_REQUEST_BRANCH
 yarn
 
 # Commit and push yarn.lock file
+git config --global user.email "no@no.com"
+git config --global user.name "Travis CI"
+git config --global push.default simple
+
 git add yarn.lock
 git commit -m "chore: update yarn.lock"
 git push "https://"$PUSH_TOKEN"@github.com/"$TRAVIS_REPO_SLUG".git"
