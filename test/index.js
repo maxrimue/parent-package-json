b0e2a02929ab1cd79a6550e90b075d9600e78ce1
maxrimue <max.rittmueller@gmail.com>
2018-04-24 23:56:49 +0200

    fix: xo linting issue (and prettier did its thing too)

--- test/index.js
+++ test/index.js
@@ -1,23 +1,37 @@
-const path = require('path');
-const test = require('ava').test;
-const parent = require('../lib/index.js');
+import path from "path";
+import { test } from "ava";
+import parent from "../lib";
 
-test('find right package.json', t => {
-	t.is(parent('./test/fixtures/firstFolder/secondFolder').path, path.normalize('test/fixtures/firstFolder/package.json'));
+test("find right package.json", t => {
+	t.is(
+		parent("./test/fixtures/firstFolder/secondFolder").path,
+		path.normalize("test/fixtures/firstFolder/package.json")
+	);
 });
 
-test('ignore one package.json', t => {
-	t.is(parent('./test/fixtures/firstFolder/secondFolder/thirdFolder/fourthFolder', 1).path, path.normalize('test/fixtures/firstFolder/package.json'));
+test("ignore one package.json", t => {
+	t.is(
+		parent(
+			"./test/fixtures/firstFolder/secondFolder/thirdFolder/fourthFolder",
+			1
+		).path,
+		path.normalize("test/fixtures/firstFolder/package.json")
+	);
 });
 
-test('read package.json', t => {
-	t.deepEqual(JSON.parse(parent('./test/fixtures/firstFolder/secondFolder').read()), {version: '1.0.0'});
+test("read package.json", t => {
+	t.deepEqual(
+		JSON.parse(parent("./test/fixtures/firstFolder/secondFolder").read()),
+		{ version: "1.0.0" }
+	);
 });
 
-test('parse package.json', t => {
-	t.deepEqual(parent('./test/fixtures/firstFolder/secondFolder').parse(), {version: '1.0.0'});
+test("parse package.json", t => {
+	t.deepEqual(parent("./test/fixtures/firstFolder/secondFolder").parse(), {
+		version: "1.0.0"
+	});
 });
 
-test('fail at finding package.json', t => {
-	t.is(parent('/'), false);
+test("fail at finding package.json", t => {
+	t.is(parent("/"), false);
 });
